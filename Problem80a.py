823d5460ab65863ee8f4f08cdcb66802c957eb49
hgparker <haroldgparker@gmail.com>
2021-03-04 13:23:38 -0800

    Solve Problem80 using Babylonian method

--- Problem80a.py
+++ Problem80a.py
@@ -4,7 +4,7 @@ from decimal import *
 from math import sqrt, floor
 from functools import reduce
 
-getcontext().prec = 110 
+getcontext().prec = 1000010
 
 def sumPlaces(num):
   root = Decimal(1)
@@ -13,7 +13,7 @@ def sumPlaces(num):
     root = (root + num/root) / Decimal(2)
     k -= 1
   rootParts = str(root).split(".")
-  return int(rootParts[0]) + reduce(lambda a, b: a+b, map(lambda q: int(q), rootParts[1][0:99]))
+  return int(rootParts[0]) + reduce(lambda a, b: a+b, map(lambda q: int(q), rootParts[1][0:999999]))
 
 sum = 0
 for k in range(2, 100):
